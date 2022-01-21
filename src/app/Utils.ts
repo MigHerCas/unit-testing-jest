db4b1617b00adb95cf9456d3f2706a446849e708
Miguel <migherdcas@gmail.com>
2022-01-21 23:48:59 +0100

    4. toEqual assertion

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,4 +1,10 @@
+import { UrlWithParsedQuery, parse } from "url";
+
 export class Utils {
+  static parseUrl(url: string): UrlWithParsedQuery {
+    return parse(url, true);
+  }
+
   static toUpperCase(arg: string) {
     return arg.toUpperCase();
   }
--- src/test/Utils.spec.ts
+++ src/test/Utils.spec.ts
@@ -7,4 +7,25 @@ describe("Utils test suite", () => {
     // Assertion toBE
     expect(result).toBe("ABC");
   });
+
+  test("Parse simple URL", () => {
+    const parsedUrl = Utils.parseUrl("http://localhost:8080/login");
+
+    expect(parsedUrl.href).toBe("http://localhost:8080/login");
+    expect(parsedUrl.port).toBe("8080");
+    expect(parsedUrl.protocol).toBe("http:");
+    expect(parsedUrl.query).toEqual({});
+  });
+
+  test("Parse simple URL with query", () => {
+    const parsedUrl = Utils.parseUrl(
+      "http://localhost:8080/login?user=user&password=pass"
+    );
+    const expectedQuery = {
+      user: "user",
+      password: "pass",
+    };
+
+    expect(parsedUrl.query).toEqual(expectedQuery);
+  });
 });
